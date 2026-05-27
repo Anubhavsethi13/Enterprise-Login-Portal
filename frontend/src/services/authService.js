799e55233ebd40093f2ddf6dbd74f6d182846b57
Anubhav Sethi <aanubhavsethi2405@gmail.com>
2026-05-27 22:43:49 +0530

    Completed enterprise JWT authentication

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,17 +1,9 @@
 import axios from "axios";
 
-const API =
-"https://loginhierarchymanagementapi.onrender.com";
+const API = "https://loginhierarchymanagementapi.onrender.com";
 
-export const loginUser =
-async (userData) => {
-
-
-  const response =
-  await axios.post(
-    `${API}/login`,
-    userData
-  );
+export const loginUser = async (userData) => {
+  const response = await axios.post(`${API}/login`, userData);
 
   return response.data;
 };
