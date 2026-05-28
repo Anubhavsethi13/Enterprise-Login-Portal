4a02bf681ffb3b2e588680635ef0ec0539de7ab9
Anubhav Sethi <aanubhavsethi2405@gmail.com>
2026-05-28 18:00:51 +0530

    userService Update

--- frontend/src/services/userService.js
+++ frontend/src/services/userService.js
@@ -2,43 +2,41 @@ import axios from "axios";
 
 const API = "https://loginhierarchymanagementapi.onrender.com";
 
-const token = localStorage.getItem("token");
+const getHeaders = () => {
+  const token = localStorage.getItem("token");
 
-const headers = {
-  Authorization: `Bearer ${token}`,
+  return {
+    Authorization: `Bearer ${token}`,
+  };
 };
 
 export const getUsers = async (page = 1) => {
-  const response = await axios.get(
-    `${API}/users?page=${page}&limit=5`,
-
-    { headers },
-  );
+  const response = await axios.get(`${API}/users?page=${page}&limit=5`, {
+    headers: getHeaders(),
+  });
 
   return response.data;
 };
 
 export const createUser = async (userData) => {
-  const response = await axios.post(
-    `${API}/users`,
-
-    userData,
-
-    { headers },
-  );
+  const response = await axios.post(`${API}/users`, userData, {
+    headers: getHeaders(),
+  });
 
   return response.data;
 };
 
 export const deleteUser = async (userId) => {
-  const response = await axios.delete(`${API}/users/${userId}`, { headers });
+  const response = await axios.delete(`${API}/users/${userId}`, {
+    headers: getHeaders(),
+  });
 
   return response.data;
 };
 
 export const updateUser = async (userId, userData) => {
   const response = await axios.put(`${API}/users/${userId}`, userData, {
-    headers,
+    headers: getHeaders(),
   });
 
   return response.data;
